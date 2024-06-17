c69be557b917979065520cf0f9a97388a38bdc61
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-06-17 19:49:21 +0530

    Day31 Dropdowns

---
+++ day31/BootstrapDropDown.java
@@ -0,0 +1,43 @@
+package day31;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BootstrapDropDown {
+
+	public static void main(String[] args)
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.manage()
+				.timeouts()
+				.implicitlyWait(Duration.ofSeconds(10));
+		
+		driver.get("https://www.jquery-az.com/boots/demo.php?ex=63.0_2");
+		driver.manage().window().maximize();
+		
+		//Open Dropdown
+		driver.findElement(By.xpath("//button[contains(@class,'multiselect')]")).click();
+		
+		//select single option
+		driver.findElement(By.xpath("//input[@value='Java']")).click();
+		
+		//capture all options
+		List<WebElement> options = driver.findElements(By.xpath("//ul//label[@class='checkbox']"));
+		System.out.println("Number Of checkboxes : "+options.size());
+		
+		//Printing all options 
+		for (WebElement webElement : options) 
+		{
+			System.out.println(webElement.getText());
+			
+		}
+		 
+		 
+	}
+
+}
--- day31/SelectDropeDown.java
+++ day31/SelectDropeDown.java
@@ -1,10 +1,13 @@
 package day31;
 
 import java.time.Duration;
+import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class SelectDropeDown {
 
@@ -20,9 +23,27 @@ public class SelectDropeDown {
 				.window()
 				.maximize();
 		
-		driver.findElement(By.xpath("//select[@id='country']")).click();
+		WebElement dropDownElement= driver.findElement(By.xpath("//select[@id='country']"));
 		
+		Select selectDrpDwn = new Select(dropDownElement);
 
+	//Select Option from drop down
+		
+		//selectDrpDwn.selectByVisibleText("Japan");
+		//selectDrpDwn.selectByValue("japan");
+		selectDrpDwn.selectByIndex(6);
+		
+	//Capture the options from the dropedown
+		
+		List<WebElement> options= selectDrpDwn.getOptions();
+		System.out.println("Number Of Options : "+options.size());
+		for (WebElement webElement : options) 
+		{
+			System.out.println(webElement.getText());
+		}
+	
+		
+		
 	}
 
 }
